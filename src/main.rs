7e9aa64901baa690250f034e2f1b1a9db214d16d
mdr4k3 <mdr4k3@proton.me>
2022-11-15 17:34:20 +0000

    Change types of variables (Use VSCODE)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let days = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "11th", "12th"];
-    let day_words = ["Two candy canes", "Three boughs of holly", "Four colored lights", "A shining star", "Little silver bells", "Candles a-glowing", "Gold and silver tinsel", "A guardian angel", "Some mistletoe", "Gifts for one and all", "All their good wishes"];
+    let days: [&str; 12] = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "11th", "12th"];
+    let day_words: [&str; 11] = ["Two candy canes", "Three boughs of holly", "Four colored lights", "A shining star", "Little silver bells", "Candles a-glowing", "Gold and silver tinsel", "A guardian angel", "Some mistletoe", "Gifts for one and all", "All their good wishes"];
 
     for i in 0..12 {
         println!("On the {} day of Christmas", days[i]);
